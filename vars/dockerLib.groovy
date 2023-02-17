8ed389a25b2141fe88c5caef27fd192e64053595
alfonsomg11 <alfonso.morales@softtek.com>
2023-02-17 14:28:16 -0600

    aaa

--- vars/dockerLib.groovy
+++ vars/dockerLib.groovy
@@ -1,5 +1,5 @@
 def build(Map params) {
-    sh 'scp -r * root@172.21.0.2:${params.build_path}'
+    sh "scp -r * root@172.21.0.2:${params.build_path}"
     sh "ssh root@172.21.0.2 'cd ${params.build_path} && docker build -t ${params.repository_url}/${params.image_name}:${params.build_number} .'"
     sh "ssh root@172.21.0.2 'cd ${params.build_path} && rm -r *'"
 }
