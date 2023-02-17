b6310df92539c0be33b12c6cb3644e7601abd29d
alfonsomg11 <alfonso.morales@softtek.com>
2023-02-17 14:58:22 -0600

    SONAR FIX

--- vars/sonarLib.groovy
+++ vars/sonarLib.groovy
@@ -2,17 +2,15 @@ def verify(Map params) {
     withCredentials([string(credentialsId: 'SONAR_TOKEN', variable: 'SONAR_TOKEN'), string(credentialsId: 'SONAR_TOKEN_DEV', variable: 'SONAR_TOKEN_DEV')]) {
         if (env.BRANCH_NAME == "development"){
             env.SONAR_TOKEN_X = "$SONAR_TOKEN_DEV"
-            env.PROJECT_KEY_X = "$PROJECT_KEY_DEV"
         }
 
         if (env.BRANCH_NAME == "master"){
             env.SONAR_TOKEN_X = "$SONAR_TOKEN"
-            env.PROJECT_KEY_X = "$PROJECT_KEY"
         }
 
         sh "mvn clean verify sonar:sonar \
             -Dsonar.projectKey=${project_key} \
             -Dsonar.host.url=${sonar_url} \
-            -Dsonar.login=${sonar_token}"
+            -Dsonar.login=$SONAR_TOKEN_X"
     }
 }
